d71bea9105362be2b368a52920fe71bbf33529d0
yashdar <yashdarji1801@gmail.com>
2023-09-06 11:28:10 -0700

    first commit

---
+++ todo-cli/__tests__/todo.js
@@ -0,0 +1,71 @@
+/* eslint-disable no-undef */
+const todoList = require("../todo");
+let today = new Date().toLocaleDateString("en-CA");
+
+const { all, markAsComplete, add, overdue, dueToday, dueLater } = todoList();
+
+describe("Todolist Testing", () => {
+  beforeAll(() => {
+    // add({
+    //   title: "DAA algorithums",
+    //   completed: false,
+    //   dueDate: new Date().toLocaleDateString("en-CA"),
+    // });
+    add({
+      title: "node js learning",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+  });
+
+  test("Add a new todo in list", () => {
+    const todoItemsCount = all.length;
+
+    // let length = all.length;
+
+    add({
+      title: "node js learning",
+      completed: false,
+      dueDate: new Date().toLocaleDateString("en-CA"),
+    });
+
+    expect(all.length).toBe(todoItemsCount+1);
+  });
+
+  test("Mark todo as a completed", () => {
+    expect(all[0].completed).toBe(false);
+    markAsComplete(0);
+    expect(all[0].completed).toBe(true);
+    
+  });
+
+  test("retrive all todos that are overdue", () => {
+    let listOfTodos = overdue();
+
+    expect(
+      listOfTodos.every((todo) => {
+        return todo.dueDate < today;
+      })
+    ).toBe(false);
+  });
+
+  test("retrive all todos that are dueToday", () => {
+    let listOfTodos = dueToday();
+
+    expect(
+      listOfTodos.every((todo) => {
+        return todo.dueDate === today;
+      })
+    ).toBe(true);
+  });
+
+  // test("retrive all todos that are dueLater", () => {
+  //   let listOfTodos = dueLater();
+
+  //   expect(
+  //     listOfTodos.every((todo) => {
+  //       return todo.dueDate > today;
+  //     })
+  //   ).toBe(true);
+  // });
+});
